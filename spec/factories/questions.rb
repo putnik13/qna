d674a3dd3757af3ec8b76c08f641d9771a29c022
putnik13 <psk379@gmail.com>
2015-06-10 22:34:58 +0600

    Question Controller && specs for it refactored 3

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :question do
     title "MyString is not to short"
-body "MyText is not to short"
+	body "MyText is not to short"
+  end
+
+  factory :invalid_question, class: Question do
+    title nil
+	body nil
   end
 
 end
