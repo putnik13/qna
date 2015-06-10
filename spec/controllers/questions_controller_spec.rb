93894c9e8b28b29a6101b178e0dd686c4fa44023
putnik13 <psk379@gmail.com>
2015-06-10 15:11:31 +0600

    Question Controller && specs for it refactored 2

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -26,4 +26,29 @@ RSpec.describe QuestionsController, type: :controller do
 			expect(response).to render_template :show
 		end
 	end
+
+	describe 'GET #new' do
+		before { get :new }
+		it 'assigns a new Question to @question' do
+			expect(assigns(:question)).to be_a_new(Question)
+		end
+		it 'renders new view' do
+			expect(response).to render_template :new
+		end
+
+	end
+
+	describe 'GET #edit' do
+		let(:question) { create(:question) }
+
+		before { get :edit, id: question }
+
+		it 'assigns the requested question to @question' do
+			expect(assigns(:question)).to eq question
+		end
+
+		it 'renders edit view' do
+			expect(response).to render_template :edit
+		end
+	end
 end
