4c7f19a52135290f34eb6fff9803c89324d2b195
putnik13 <psk379@gmail.com>
2015-06-15 14:32:25 +0600

    problems solved. Answers are done

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
+  
   factory :answer do
-    title "MyString"
-body "MyText"
-question_id 1
+    title "MyString23"
+	body "MyText MyText MyText"
+	question_id 1
   end
 
+
+  factory :invalid_answer, class: Answer do
+    title nil
+	body nil
+  end
 end
